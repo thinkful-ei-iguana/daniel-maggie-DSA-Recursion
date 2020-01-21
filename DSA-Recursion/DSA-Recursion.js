8d3e287f8c924824b7f086964abf36b5c5eee40b
therealmbittarelli <maggiekmcclellan@gmail.com>
2020-01-21 11:37:15 -0500

    finished exercise one

---
+++ DSA-Recursion/DSA-Recursion.js
@@ -0,0 +1,12 @@
+// function sheepJump(sheep) {
+//   if (sheep === 0) {
+//     return console.log('All sheep jumped over the fence');
+//   }
+//   console.log(`${sheep}: Another sheep jumps over the fence`);
+//   sheepJump(sheep - 1);
+// }
+
+// sheepJump(3);
+
+///////////////////
+
