5ca9b5f046e49dd6a70a6b530b07563561ac4a3e
Daniel Kent <Kentpercussion@gmail.com>
2020-01-21 10:46:32 -0600

    powers

--- DSA-Recursion/DSA-Recursion.js
+++ DSA-Recursion/DSA-Recursion.js
@@ -1,12 +1,20 @@
-// function sheepJump(sheep) {
-//   if (sheep === 0) {
-//     return console.log('All sheep jumped over the fence');
-//   }
-//   console.log(`${sheep}: Another sheep jumps over the fence`);
-//   sheepJump(sheep - 1);
-// }
+function sheepJump(sheep) {
+  if (sheep === 0) {
+    return console.log('All sheep jumped over the fence');
+  }
+  console.log(`${sheep}: Another sheep jumps over the fence`);
+  sheepJump(sheep - 1);
+}
+sheepJump(3);
 
-// sheepJump(3);
+function powerCalc(base, exp) {
+  if (exp <= 0) return -1;
+  //base
+  if (exp === 1) return base;
 
-///////////////////
+  //recursion
+  return base * (powerCalc(base, exp-1));
+}
+console.log(powerCalc(2, 2));
+
 
